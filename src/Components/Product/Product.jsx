cfff6844323917f0813d0a054ab2ddab8a578b97
Shubham <shubham101021@gmail.com>
2022-10-20 11:57:50 +0530

    some more changes

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,99 +1,110 @@
-import React from 'react'
-import "./Products.css"
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Products.css";
 
 const Product = () => {
+  return (
+    <section className="module" id="alt-features">
+      <div className="container py-5">
+        <div className="py-5">
+          <div className="row">
+            <div className="col-sm-12 h-100">
+              {/* <h2 className="module-title font-alt">Our Products</h2> */}
+              <div className="module-subtitle font-serif">
+                What we do Produce
+                <br />
+                To Make Your Home More Smiling
+              </div>
 
-    return (
-        <section className="module" id="alt-features">
-            <div className="container py-5">
-                <div className="py-5">
-                    <div className="row">
-                        <div className="col-sm-12 h-100">
-                            {/* <h2 className="module-title font-alt">Our Products</h2> */}
-                            <div className="module-subtitle font-serif">What we do Produce<br />
-                                To Make Your Home More Smiling</div>
-
-                            <div className="col-lg-3 mb-3 mb-lg-0 h-100">
-                                <div className="hover hover-2 text-white rounded">
-                                    <img
-                                        src="https://i.pinimg.com/564x/f4/c2/19/f4c2191e397e8b9a69768f8517c0d5ca.jpg"
-                                        alt=""
-                                    />
-                                    <div className="hover-overlay" />
-                                    <div className="hover-2-content px-5 py-4">
-                                        <h3 className="hover-2-title text-uppercase font-weight-bold mb-0">
-
-                                            <span className="font-weight-light text-white">Neo </span>PRO
-                                        </h3>
-                                        <p className="hover-2-description text-uppercase mb-0">
-                                            Read More
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="col-lg-3" style={{paddingTop : '30px'}}>
-                                <div className="hover hover-2 rounded">
-                                    <img
-                                        src="https://i.pinimg.com/564x/5e/3c/e8/5e3ce850846275ededc6e32ad1cb8293.jpg"
-                                        alt=""
-                                    />
-                                    <div className="hover-overlay" />
-                                    <div className="hover-2-content px-5 py-4">
-                                        <h3 className="hover-2-title text-uppercase font-weight-bold mb-0">
-                                            {" "}
-                                            <span className="text-white">NEO </span>Sink
-                                        </h3>
-                                        <p className="hover-2-description text-uppercase mb-0">
-                                            Read More
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="col-lg-3">
-                                <div className="hover hover-2 text-white rounded">
-                                    <img
-                                        src="https://i.pinimg.com/564x/99/71/4b/99714b98136c353f79f3cb367d231220.jpg"
-                                        alt=""
-                                    />
-                                    <div className="hover-overlay" />
-                                    <div className="hover-2-content px-5 py-4">
-                                        <h3 className="hover-2-title text-uppercase font-weight-bold mb-0">
-                                            {" "}
-                                            <span className="font-weight-light">NEO </span>Bath
-                                        </h3>
-                                        <p className="hover-2-description text-uppercase mb-0">
-                                            Read More
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="col-lg-3 " style={{paddingTop : '30px'}}>
-                                <div className="hover hover-2 text-white rounded">
-                                    <img
-                                        src="https://i.pinimg.com/564x/e2/e3/5b/e2e35b6e32e2dbb5b730a092be3fd02a.jpg"
-                                        alt=""
-                                    />
-                                    <div className="hover-overlay" />
-                                    <div className="hover-2-content px-5 py-4">
-                                        <h3 className="hover-2-title text-uppercase font-weight-bold mb-0">
-                                            {" "}
-                                            <span className="font-weight-light">NEO </span>Cabinets
-                                        </h3>
-                                        <p className="hover-2-description text-uppercase mb-0">
-                                            Read More
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
+              <div className="col-lg-3 mb-3 mb-lg-0 h-100">
+                <Link to="/NeoPro">
+                  <div className="hover hover-2 text-white rounded">
+                    <img
+                      src="https://i.pinimg.com/564x/f4/c2/19/f4c2191e397e8b9a69768f8517c0d5ca.jpg"
+                      alt=""
+                    />
+                    <div className="hover-overlay" />
+                    <div className="hover-2-content px-5 py-4">
+                      <h3 className="hover-2-title text-uppercase font-weight-bold mb-0">
+                        <span className="font-weight-light text-white">
+                          Neo{" "}
+                        </span>
+                        PRO
+                      </h3>
+                      <p className="hover-2-description text-uppercase mb-0">
+                        Read More
+                      </p>
                     </div>
+                  </div>
+                </Link>
+              </div>
 
-                    {/* <div className="py-5">
+              <div className="col-lg-3" style={{ paddingTop: "30px" }}>
+                <Link to="/NeoSink">
+                  <div className="hover hover-2 rounded">
+                    <img
+                      src="https://i.pinimg.com/564x/5e/3c/e8/5e3ce850846275ededc6e32ad1cb8293.jpg"
+                      alt=""
+                    />
+                    <div className="hover-overlay" />
+                    <div className="hover-2-content px-5 py-4">
+                      <h3 className="hover-2-title text-uppercase font-weight-bold mb-0">
+                        {" "}
+                        <span className="text-white">NEO </span>Sink
+                      </h3>
+                      <p className="hover-2-description text-uppercase mb-0">
+                        Read More
+                      </p>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+
+              <div className="col-lg-3">
+                <Link to="/NeoBath">
+                  <div className="hover hover-2 text-white rounded">
+                    <img
+                      src="https://i.pinimg.com/564x/99/71/4b/99714b98136c353f79f3cb367d231220.jpg"
+                      alt=""
+                    />
+                    <div className="hover-overlay" />
+                    <div className="hover-2-content px-5 py-4">
+                      <h3 className="hover-2-title text-uppercase font-weight-bold mb-0">
+                        {" "}
+                        <span className="font-weight-light">NEO </span>Bath
+                      </h3>
+                      <p className="hover-2-description text-uppercase mb-0">
+                        Read More
+                      </p>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+
+              <div className="col-lg-3 " style={{ paddingTop: "30px" }}>
+                <Link to="/NeoCabinate">
+                  <div className="hover hover-2 text-white rounded">
+                    <img
+                      src="https://i.pinimg.com/564x/e2/e3/5b/e2e35b6e32e2dbb5b730a092be3fd02a.jpg"
+                      alt=""
+                    />
+                    <div className="hover-overlay" />
+                    <div className="hover-2-content px-5 py-4">
+                      <h3 className="hover-2-title text-uppercase font-weight-bold mb-0">
+                        {" "}
+                        <span className="font-weight-light">NEO </span>Cabinets
+                      </h3>
+                      <p className="hover-2-description text-uppercase mb-0">
+                        Read More
+                      </p>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
+
+          {/* <div className="py-5">
                 <h3 className="font-weight-bold mb-0">Demo 3</h3>
                 <p className="font-italic text-muted mb-4">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -139,13 +150,10 @@ const Product = () => {
                     </div>
                 </div>
             </div> */}
+        </div>
+      </div>
+    </section>
+  );
+};
 
-
-                </div>
-            </div>
-        </section>
-
-    )
-}
-
-export default Product
+export default Product;
